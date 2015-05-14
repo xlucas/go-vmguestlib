915702c76415a8374627d2484c9231dff7727a33
xlucas <xavier.lucas.2@gmail.com>
2015-05-14 13:54:10 -0500

    VMware native library headers

---
+++ native/vmSessionId.h
@@ -0,0 +1,21 @@
+/* **********************************************************
+ * Copyright (C) 2006-2015 VMware, Inc.  All rights reserved. -- VMware Confidential
+ * **********************************************************/
+
+#ifndef _VM_SESSION_ID_H_
+#define _VM_SESSION_ID_H_
+
+
+#include "vm_basic_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef uint64 VMSessionId;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _VM_SESSION_ID_H_ */
