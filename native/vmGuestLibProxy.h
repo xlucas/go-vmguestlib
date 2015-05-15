2330fa69dc071f1eb8320ea39c93b5aa3010c4f6
xlucas <xavier.lucas.2@gmail.com>
2015-05-15 11:58:32 -0500

    Comments in native proxy library

--- native/vmGuestLibProxy.h
+++ native/vmGuestLibProxy.h
@@ -1,3 +1,15 @@
+/*
+ * Product : go-vmguestlib
+ * Date : 2015
+ * Author : Xavier Lucas
+ * Licence : GNU GPL2
+ *
+ * These functions permits to workaround the missing implementation of C function pointers
+ * in Go. We define a new pointer type for each native function return type and declare
+ * a proxy function per return type. All native target functions must have the same
+ * parameters than the proxy function calling them.
+ */
+
 #ifndef _VM_GUEST_LIB_PROXY_H
 #define _VM_GUEST_LIB_PROXY_H
 
