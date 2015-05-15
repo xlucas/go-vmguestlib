7e9025e778eef4dabb22d8c6c2cfbcad49bfa9cf
xlucas <xavier.lucas.2@gmail.com>
2015-05-14 19:21:34 -0500

    Session object is the main one. First getter and tests.

--- vmguestlib/session.go
+++ vmguestlib/session.go
@@ -7,6 +7,7 @@ package vmguestlib
 #include <vmSessionId.h>
 */
 import "C"
+import "time"
 
 // A VMGuestLib session.
 //
@@ -25,32 +26,68 @@ import "C"
 // of previously retrieved information.
 //
 type Session struct {
+	Handle        *Handle
 	NativeSession *C.VMSessionId
 }
 
-// NewSession opens a new session.
-func NewSession(h *Handle) (s *Session, err error) {
+// NewSession opens a new session from the given handle.
+func Open(h *Handle) (s *Session, err error) {
 	s = &Session{
+		Handle:        h,
 		NativeSession: new(C.VMSessionId),
 	}
-	err = h.UpdateInfo()
+	_, err = s.RefreshInfo()
+	return
+}
+
+// Close teardown the underlying handle.
+func (s *Session) Close() error {
+	return s.Handle.Close()
+}
+
+// NewSession allocates a new handle and calls Open().
+func NewSession() (s *Session, err error) {
+	h, err := NewHandle()
 	if err != nil {
 		return
 	}
-	_, err = s.Refresh(h)
+	s, err = Open(h)
 	return
 }
 
 // Refresh updates the session if the virtual machine session
 // changed on the host. In this case, hasChanged is set to true.
-func (s *Session) Refresh(h *Handle) (hasChanged bool, err error) {
+func (s *Session) Refresh() (hasChanged bool, err error) {
 	newSession := new(C.VMSessionId)
-	e := C.VMGuestLib_GetSessionId(*h.NativeHandle, newSession)
+	e := C.VMGuestLib_GetSessionId(*s.Handle.NativeHandle, newSession)
 	if e != ERROR_SUCCESS {
 		err = NewError(e)
 		return
 	}
 	hasChanged = (*newSession != *s.NativeSession)
 	s.NativeSession = newSession
+	return
+}
+
+// RefreshInfo calls UpdateInfo() on the handler then call
+// Refresh() on the session.
+func (s *Session) RefreshInfo() (hasChanged bool, err error) {
+	err = s.Handle.UpdateInfo()
+	if err != nil {
+		return
+	}
+	hasChanged, err = s.Refresh()
+	return
+}
+
+// GetElaspedTime retrieves the duration since the virtual machine
+// started running in the current host system.
+func (s *Session) GetElaspedTime() (t time.Duration, err error) {
+	elapsedMs := new(C.uint64)
+	e := C.VMGuestLib_GetElapsedMs(*s.Handle.NativeHandle, elapsedMs)
+	if e != ERROR_SUCCESS {
+		err = NewError(e)
+	}
+	t = time.Duration(*elapsedMs) * time.Millisecond
 	return
 }
--- vmguestlib/session_test.go
+++ vmguestlib/session_test.go
@@ -2,16 +2,38 @@ package vmguestlib
 
 import "testing"
 
-func TestNewSession(t *testing.T) {
+func TestOpenAndCloseSession(t *testing.T) {
 	h, err := NewHandle()
 	if err != nil {
 		t.Fatal(err)
 	}
-	_, err = NewSession(h)
+	s, err := Open(h)
 	if err != nil {
 		t.Fatal(err)
 	}
-	err = h.Close()
+	err = s.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestNewSessionAndClose(t *testing.T) {
+	s, err := NewSession()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = s.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGetElapsedTime(t *testing.T) {
+	s, err := NewSession()
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = s.GetElaspedTime()
 	if err != nil {
 		t.Fatal(err)
 	}
